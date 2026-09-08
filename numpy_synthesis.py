525521ee507496b6313de2ec3cf77955edd35461
DDreKAY <jeeyul2010@gmail.com>
2026-09-08 21:28:02 +0900

    numpy basics v0.4

--- numpy_synthesis.py
+++ numpy_synthesis.py
@@ -1,14 +1,23 @@
 import numpy as np
 
-array01 = np.zeros((3,4))
+array01 = np.ones((3,4))
+array02 = np.array([[1,2,3,10],[4,5,6,11],[7,8,9,12]])
 
 print(array01)
-print(array01.shape)
-print(array01.size)
-print(array01.ndim)
-array01[2,1] = 7.0
-array01[1,2] = 11.0
-print(array01)
-print(array01[1:, [1]])
-print(array01[1:, 1:3])
-print(array01[array01<=7])
+array01[0,0] = 7.0
+
+print(array01+array02)
+array01=array01.T
+array01=array01.reshape(6,2)
+array02=array02.reshape(2,6)
+print(array01 + 4.1)
+print(np.sum(array01))
+print(np.max(array01))
+print(np.min(array02))
+print(array01@array02)
+array03=array01@array02
+print(array03.flatten())
+
+array03 = np.arange(1,11,1)
+array04 = np.linspace(0,1,10)
+print(array04.dtype)

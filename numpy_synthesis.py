6ea137861c0ebb4256cd2879c5652253334d674f
DDreKAY <jeeyul2010@gmail.com>
2026-09-08 20:27:29 +0900

    numpy basics v0.2

--- numpy_synthesis.py
+++ numpy_synthesis.py
@@ -1,9 +1,17 @@
 import numpy as np
 
 # 배열 생성
-np_array = np.array([3,'2',1.7])
-print(np_array, type(np_array),type(np_array[0]))
+np_array01 = np.array([3,2,1])
+np_array02 = np.zeros((2,3))
+np_array03 = np.ones((3,3))
+#np_array04 = np.arange(0,11,2)
+np_array04 = np.arange(5)
+array05 = np.linspace(0,1,4)
 
-#python 리스트 생성
-list_array = [3,'2',1.7]
-print(list_array, type(list_array),type(list_array[0]))
+print(np_array01)
+print(np_array02)
+np_array02[1,1] = 9.0
+print(np_array02, type(np_array02))
+print(np_array03)
+print(np_array04)
+print(array05)

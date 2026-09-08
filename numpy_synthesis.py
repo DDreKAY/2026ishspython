09f654fec4ef64a19182ff0bd4b10f9673eacbfe
DDreKAY <jeeyul2010@gmail.com>
2026-09-08 20:39:23 +0900

    numpy basics v0.4

--- numpy_synthesis.py
+++ numpy_synthesis.py
@@ -1,16 +1,14 @@
 import numpy as np
 
-# 배열 생성
-np_array01 = np.array([3,2,1])
-np_array02 = np.zeros((2,3))
-np_array03 = np.ones((3,3))
-#np_array04 = np.arange(0,11,2)
-np_array04 = np.arange(5)
-array05 = np.linspace(0,1,5)
+array01 = np.zeros((3,4))
 
-# 배열 속성 (attribute)
-print(np_array01.shape) #배열 모양 (행, 열, (면))
-print(np_array02.shape)
-print(np_array03.ndim) # 차원수
-print(np_array04.dtype) #데이터 타입
-print(array05.size) #전체 원소 개수
+print(array01)
+print(array01.shape)
+print(array01.size)
+print(array01.ndim)
+array01[2,1] = 7.0
+array01[1,2] = 11.0
+print(array01)
+print(array01[1:, [1]])
+print(array01[1:, 1:3])
+print(array01[array01<=7])

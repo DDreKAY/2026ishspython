17719ce9504e604943d3aa745a26edb02da0c244
DDreKAY <jeeyul2010@gmail.com>
2026-09-08 20:20:43 +0900

    numpy basics v0.1

---
+++ numpy_synthesis.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# 배열 생성
+np_array = np.array([3,'2',1.7])
+print(np_array, type(np_array),type(np_array[0]))
+
+#python 리스트 생성
+list_array = [3,'2',1.7]
+print(list_array, type(list_array),type(list_array[0]))
